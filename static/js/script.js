86f7c423fb4c5af203d60c18e95a63b6212e27fe
erlan <erlanmetaka1996@gmail.com>
2021-11-26 08:58:47 +0700

    Javascript Basic

--- static/js/script.js
+++ static/js/script.js
@@ -26,16 +26,23 @@ function generateCat() {
 //Challenge 3: Rock, Paper, Scissors
 
 function rpsGame(yourChoice) {
+  // console.log(yourChoice);
   var humanChoice, botChoice;
   humanChoice = yourChoice.id;
-  botChoice = numberToChoice(randomRps());
-  results = decideWinner(humanChoice, botChoice);
-  message = finalMessage(results);
-  rpsFrontEnd(yourChoice.id, botChoice, message);
   
-} 
+  botChoice = numberToChoice(randToRpsInt());
+  // console.log('Computer choice:', botChoice);
 
-function randomRps() {
+  results = decideWinner(humanChoice, botChoice);
+  // console.log(results);
+
+  message = finalMessage(results);
+  // console.log(message);
+
+  rpsFrontEnd(yourChoice.id, botChoice, message)
+}
+
+function randToRpsInt() {
   return Math.floor(Math.random() * 3);
 }
 
@@ -44,33 +51,33 @@ function numberToChoice(number) {
 }
 
 function decideWinner(yourChoice, computerChoice) {
-  var rpsDb = {
-    'rock': {'scissors':1, 'rock':0.5, 'paper':0},
-    'paper': {'rock':1, 'paper':0.5, 'scissors':0},
-    'scissors': {'paper':1, 'scissors':0.5, 'rock':0}
+  var rpsDatabase = {
+      'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
+      'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
+      'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0}
   }
 
-  var yourScore = rpsDb[yourChoice][computerChoice];
-  var computerScore = rpsDb[computerChoice][yourChoice];
+  var yourScore = rpsDatabase[yourChoice][computerChoice];
+  var computerScore = rpsDatabase[computerChoice][yourChoice];
 
-  return [yourScore, computerScore];
+  return [yourScore,computerScore];
 }
 
 function finalMessage([yourScore, computerScore]) {
   if (yourScore === 0) {
-    return {'message': 'Mampus', 'color': 'red'};
+      return {'message': 'Dumb!', 'color': 'red'};
   } else if (yourScore === 0.5) {
-    return {'message': 'Seri', 'color': 'gray'};
+      return {'message': 'You Tied!', 'color': 'yellow'};
   } else {
-    return {'message': 'Hore', 'color': 'black'};
+      return {'message': 'Not Bad!', 'color': 'green'};
   }
 }
 
-function rpsFrontEnd(humanImg, botImg, finalMessage) {
-  var imagesDb = {
-    'rock': document.getElementById('rock').src,
-    'paper': document.getElementById('paper').src,
-    'scissors': document.getElementById('scissors').src
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
+  var imagesDatabase = {
+      'rock': document.getElementById('rock').src,
+      'paper': document.getElementById('paper').src,
+      'scissors': document.getElementById('scissors').src
   }
 
   document.getElementById('rock').remove();
@@ -78,14 +85,15 @@ function rpsFrontEnd(humanImg, botImg, finalMessage) {
   document.getElementById('scissors').remove();
 
   var humanDiv = document.createElement('div');
-  var messageDiv = document.createElement('div');
   var botDiv = document.createElement('div');
+  var messageDiv = document.createElement('div');
 
-  humanDiv.innerHTML = "<img src='" + imagesDb[humanImg] + "' height=150 width=150 style='box-shadow: 0px 10px 58px rgba(37, 58, 223, 1);'>"
-  messageDiv.innerHTML = "<h1 style='color " + finalMessage['color'] + "; font-size: 50px; padding: 30px; '>" + finalMessage['mesage'] + "</h1>"
-  botDiv.innerHTML = "<img src='" + imagesDb[botImg] + "' height=150 width=150 style='box-shadow: 0px 10px 58px rgba(243, 38, 24, 1);'>"
+  console.log('ini ' + imagesDatabase)
+
+  humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>"
+  messageDiv.innerHTML = "<h1 style='color " + finalMessage['color'] + "; font-size: 50px; padding:30px; '>" + finalMessage['message'] + "</h1>"
+  botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243, 38, 24, 1);'>"
   
-
   document.getElementById('flex-box-rps-div').appendChild(humanDiv);
   document.getElementById('flex-box-rps-div').appendChild(messageDiv);
   document.getElementById('flex-box-rps-div').appendChild(botDiv);
