77895b19501705343429f7a44f956ab37eec3cac
erlan <erlanmetaka1996@gmail.com>
2021-11-27 13:44:36 +0700

    JS beginner

--- static/js/script.js
+++ static/js/script.js
@@ -101,5 +101,40 @@ function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
 
 // Challenge 4: BlackJack
 
+let blackjackGame = {
+  'you' : {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score':0},
+  'dealer' : {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score':0},
+};
 
+const YOU = blackjackGame['you']
+const DEALER = blackjackGame['dealer']
 
+const hitSound = new Audio('static/sounds/swish.m4a');
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+
+function blackjackHit() {
+  showCard(YOU);  
+}
+
+function showCard(activePlayer) {
+  let cardImage = document.createElement('img');
+  cardImage.src = 'static/images/black_joker.png';
+  document.querySelector(activePlayer['div']).appendChild(cardImage);
+  hitSound.play();
+}
+
+function blackjackDeal() {
+  let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+  let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
+  // console.log(yourImages);
+  for (i=0; i < yourImages.length; i++) {
+    yourImages[i].remove();
+  }
+
+  for (i=0; i < dealerImages.length; i++) {
+    dealerImages[i].remove();
+  }
+}
